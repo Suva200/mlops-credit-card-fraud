ee2c58fca01b0f3a22673a180902852d315bc01c
SUVARANAMALIYA JOTHIBABU <suvaranamaliya1999@gmail.com>
2026-02-27 18:10:33 +0100

    loaded  trained model artifacts for inference and API integration

--- src/api.py
+++ src/api.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from src.schemas import PredictionRequest, PredictionResponse
 import joblib
+from src.inference import ModelLoader
+
+
 
 app = FastAPI(title="Credit Card Fraud Detection API")
 
-model = joblib.load("artifacts/best_model.pkl")
+## model = joblib.load("artifacts/best_model.pkl")
+model = ModelLoader.load_model()
 
 @app.get("/health")
 def health():
