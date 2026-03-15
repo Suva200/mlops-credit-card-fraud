b38bb56399e004ccce2aa37e613312b242991137
VisshnuPrethi <89800914+VisshnuPrethi@users.noreply.github.com>
2026-03-15 18:12:25 +0100

    Added Logging

--- src/api.py
+++ src/api.py
@@ -1,17 +1,33 @@
 from fastapi import FastAPI
 from src.schemas import PredictionRequest, PredictionResponse
-import joblib
 from src.inference import ModelLoader
+import logging
+import joblib
 
+#  Logging setup
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+#  App & model initialization 
 app = FastAPI(title="Credit Card Fraud Detection API")
 
+logger.info("Loading fraud detection model...")
 model = ModelLoader.load_model()
+logger.info("Model loaded successfully.")
 
+
+#  Routes 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
+
 @app.post("/predict", response_model=PredictionResponse)
 def predict(request: PredictionRequest):
+    logger.info("Prediction request received")
     prediction = model.predict([request.features])
+    logger.info(f"Prediction result: {prediction[0]}")
     return {"fraud_prediction": int(prediction[0])}
